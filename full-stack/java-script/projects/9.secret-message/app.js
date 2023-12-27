d1a6a673ecd1fb1f7c4ddca63a7b2f98cac29fa4
asmfish <asmeromf2014@gmail.com>
2023-12-27 23:43:13 +0000

    Update app.js

--- full-stack/java-script/projects/9.secret-message/app.js
+++ full-stack/java-script/projects/9.secret-message/app.js
@@ -1,1 +1,30 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+console.log('Initial length: ' + secretMessage.length);
+//Remove last
+secretMessage.pop();
+console.log(secretMessage.length);
+
+//Add
+secretMessage.push('to', 'program');
+console.log(secretMessage.length);
+
+//Change
+secretMessage[7] = 'right';
+
+//Remove first
+secretMessage.shift();
+console.log(secretMessage.length);
+
+//Add first
+secretMessage.unshift('Programming');
+console.log(secretMessage.length);
+
+//Remove multiple
+secretMessage.splice(6, 5, "know");
+console.log(secretMessage.length);
+
+//Print
+console.log(secretMessage.join(' '));
+
 
