a600ab5fda81193de04069724adf7fe1ee5e8408
asmfish <asmeromf2014@gmail.com>
2023-12-27 23:46:42 +0000

    Update app.js

--- full-stack/java-script/projects/11.meal-maker/app.js
+++ full-stack/java-script/projects/11.meal-maker/app.js
@@ -1,1 +1,27 @@
+const menu = {
+  _meal: '',
+  _price: 0,
+  set meal(mealToCheck){
+    if(typeof(mealToCheck) === 'string'){
+       return this._meal = mealToCheck;
+    }
+  },
+  set price(priceToCheck){
+    if(typeof(priceToCheck) === 'number'){
+      return this._price = priceToCheck;
+    }
+  },
+  get todaysSpecial(){
+    if(this._meal && this._price){
+      return `Today's Special is ${this._meal} for $${this._price}!`;
+    }
+    else{
+      return 'Meal or price was not set correctly!';
+    }
+  }
+}
+
+menu.meal = 'Sphagetii Italiano';
+menu.price = 13.5;
+console.log(menu.todaysSpecial)
 
