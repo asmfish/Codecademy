f4425d4d3153d93171ff80f42a6c8f3626d18912
asmfish <asmeromf2014@gmail.com>
2023-12-27 23:45:32 +0000

    Update app.js

--- full-stack/java-script/projects/10.whale-talk/app.js
+++ full-stack/java-script/projects/10.whale-talk/app.js
@@ -1,1 +1,22 @@
+let input = 'turpentine and turtles';
+const vowels = ['a','e','i','o','u'];
+let resultArray = [];
 
+for(let i = 0 ; i < input.length; i++){
+  //console.log(input[i]);
+  for(let j = 0 ; j < vowels.length; j++){
+    //console.log(vowels[j]);
+    if(input[i] === vowels[j]){
+      //console.log(input[i])
+      resultArray.push(input[i]);
+      if(input[i] === 'e' || input[i] === 'u'){
+        resultArray.push(input[i]);
+      }
+    }
+  }
+}
+
+console.log(resultArray)
+
+const resultString = resultArray.join('').toUpperCase();
+console.log(resultString)
